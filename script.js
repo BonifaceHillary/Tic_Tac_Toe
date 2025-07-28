3415d73eeae2817e6abab3cc45bd919090b54560
Boniface Hillary <nkasiboniface@gail.com>
2025-07-28 23:12:49 +0100

    initial commit

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ const App = {
     });
 
     return {
-      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
-      winner, // 1 | 2 | null
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress",
+      winner, 
     };
   },
  
